648d29bda057efac57ed15585398b7302a1ba1cd
Michael McCormick <MichaelM97@live.co.uk>
2019-11-07 20:45:52 +0000

    Created Registration JSP and Servlet

---
+++ XYZDriverAssociation/src/main/java/servlet/auth/Registration.java
@@ -0,0 +1,57 @@
+package servlet.auth;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author michaelmccormick
+ */
+public class Registration extends HttpServlet {
+
+    /**
+     * Displays the registration JSP.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher view = request.getRequestDispatcher("auth/registration.jsp");
+        view.forward(request, response);
+    }
+
+    /**
+     * Submits the users entered information to the DB and creates a new
+     * account.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        // TODO: Await DB setup in order to authenticate the user using the above parameters
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Registers a new users account.";
+    }
+}
---
+++ XYZDriverAssociation/src/main/webapp/auth/registration.jsp
@@ -0,0 +1,27 @@
+<%-- 
+    Document   : registration
+    Created on : 07-Nov-2019, 20:41:18
+    Author     : michaelmccormick
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Registration</title>
+    </head>
+    <body>
+        <h1>Registration portal</h1>
+        <form action='Registration' method="post">
+            <h4>Username:</h4>
+            <input type="text" name="username" placeholder="Choose a username"/>
+            <br>
+            <h4>Password:</h4>
+            <input type="password" name="password" placeholder="Choose a password"/>
+            <br>
+            <br>
+            <input name='submitRegistrationButton' type='submit' value='Create account'/>
+        </form>
+    </body>
+</html>
