69361bf74dcae6229a3b2728401a416273dcf981
Michael McCormick <MichaelM97@live.co.uk>
2019-11-07 20:40:15 +0000

    * Created AdminLogin JSP and Servlet. * Add NetBeans configuration file to .gitignore

---
+++ XYZDriverAssociation/src/main/java/servlet/auth/AdminLogin.java
@@ -0,0 +1,56 @@
+package servlet.auth;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author michaelmccormick
+ */
+public class AdminLogin extends HttpServlet {
+
+    /**
+     * Displays the admin_login JSP.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher view = request.getRequestDispatcher("auth/admin_login.jsp");
+        view.forward(request, response);
+    }
+
+    /**
+     * Submits the users entered information to the DB and authenticates login.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        // TODO: Await DB setup in order to authenticate the user using the above parameters
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Handles an administrator login.";
+    }
+}
