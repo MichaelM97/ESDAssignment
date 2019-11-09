51dc0e2c5b220fe01f11e3c9ce46e30e9caeed19
Dominic Lewis <dominiclewis96@gmail.com>
2019-11-09 18:18:39 +0000

    Initialises Database

--- XYZDriverAssociation/src/main/java/db/Database.java
+++ XYZDriverAssociation/src/main/java/db/Database.java
@@ -5,49 +5,123 @@
  */
 package db;
 import java.sql.*;
+
+import java.io.FileReader;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
 /**
  * Initialises the Database
  */
-public class DatabaseInit {
-    static Connection conn = null;
-    static Statement statement = null;
-    static ResultSet resultSet = null;
+public class Database {
+    private static Database instance = null;
+    private static Connection conn = null;
+    private final String DERBY_URL = ("jdbc:derby://localhost:1527/"
+            + "XYZDriverAssociation;create=true");
+    private final String DB_INIT_JSON = "src/main/java/db/init_db.json";
+    private final String ROOT_USR = "root";
+    private final String ROOT_PW = "password";
 
-    DatabaseInit(){
-        initDB();
+    private Database(){
+        //Establish Connection
+        init_tables();
+        
     }
-    
- private static void initDB(){
-      
-     String foo = ("CREATE TABLE users ("
-              + "UID INT NOT NULL,"
-              + "NAME VARCHAR(45) NOT NULL)");
-     try {
-        conn = DriverManager.getConnection(
-        "jdbc:derby://localhost:1527/XYZDriverAssociation;create=true",
-                "root", "password");
-     } catch (SQLException ex){
-         // Log out the exception
-         System.out.println(ex);
-     }
 
-     try {
-         if (conn != null){
-             statement = conn.createStatement();
-             statement.executeUpdate(foo); 
-             statement.close();
-             conn.close();
-         }
-     } catch (SQLException ex){
-         System.out.println("Create Fail");
+    /**
+    * Singleton DB creation pattern.
+    * @return Database -
+    *   Thread Safe Singleton instance of a DB with access methods.
+    */
+    public static Database get_DB(){
+        // Thread Safe
+        synchronized(Database.class){
+            if(instance == null){
+                instance = new Database();
+            }
+            return instance;
         }
-     
-     }
+    }
+
+    /**
+    * Singleton Connection creation pattern.
+    * @return Connection - 
+    *   Thread Safe Singleton instance of a connection.
+    */
+    private Connection getConn() {
+        // Thread Safe 
+        synchronized(this){
+            if(conn == null){
+                try{
+                    conn = DriverManager.getConnection(
+                            DERBY_URL, ROOT_USR, ROOT_PW);
+                } catch (SQLException ex){
+                    //log it out
+                }
+            }
+        }
+        return conn;
+        }
+
+    /**
+    * Initialises the Database with relevant tables read in from init_db.json
+    */
+    private void init_tables(){
+        JSONObject j_reader = null;
+        // Load JSON DB initialisation layout
+        try{
+            j_reader = (JSONObject) new JSONParser().parse(
+                new FileReader(DB_INIT_JSON));
+        } catch(IOException | ParseException ex){
+            System.out.println(ex);
+        }
+        // Loop through table names
+        for (Object table_name: j_reader.keySet()){
+            String sql = "CREATE TABLE ";
+            boolean first = true;
+            // Read columns from json by using table name
+             sql += (String) table_name + "(";
+             Map columns = (Map)j_reader.get(table_name);
+             // Generate iterator which contains k,v pairs for sets of table
+             // fields.
+             Iterator<Map.Entry> cols = columns.entrySet().iterator();
+             while (cols.hasNext()){
+                 if (first == false){
+                     sql += ',';
+                 }
+                 first = false;
+                 Map.Entry pair = cols.next();
+                 sql+= ("\n " + pair.getKey() + " " + pair.getValue());
+             }
+             sql += ")";
+             try{
+             execute_sql(sql);
+             } catch(SQLException ex){
+                 System.out.println(ex);
+             }
+        }
+    }
+    /**
+    * Private method for adding SQL to database, this should not be used
+    * generally.
+    */
+    private void execute_sql(String sql) throws SQLException{
+        // Log out what's being created
+        System.out.println(sql);
+        getConn();
+        conn.createStatement().executeUpdate(sql);
+    }
+
 
  public static void main(String[] args){
-    new DatabaseInit();
+    new Database();
      
  }
  }
-
-
