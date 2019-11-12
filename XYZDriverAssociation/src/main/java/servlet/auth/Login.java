0f53f22ce7779f58597ea88d0af8d217103bf3bd
Michael McCormick <MichaelM97@live.co.uk>
2019-11-12 10:44:38 +0000

    Removed unused imports

--- XYZDriverAssociation/src/main/java/servlet/auth/Login.java
+++ XYZDriverAssociation/src/main/java/servlet/auth/Login.java
@@ -11,7 +11,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import model.User;
 import utils.HashHelper;
 import utils.SessionHelper;
--- XYZDriverAssociation/src/main/java/servlet/auth/Registration.java
+++ XYZDriverAssociation/src/main/java/servlet/auth/Registration.java
@@ -7,7 +7,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import model.User;
 import utils.HashHelper;
 import utils.SessionHelper;
