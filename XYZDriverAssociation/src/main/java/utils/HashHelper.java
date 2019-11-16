0fb369a12a7b20208d0ca962acefe24448132a07
Michael McCormick <MichaelM97@live.co.uk>
2019-11-16 19:05:02 +0000

    Added comments to HashHelper's generateHash function

--- XYZDriverAssociation/src/main/java/utils/HashHelper.java
+++ XYZDriverAssociation/src/main/java/utils/HashHelper.java
@@ -25,10 +25,15 @@ public class HashHelper {
         StringBuilder hash = new StringBuilder();
 
         try {
+            // Generate bytes from the textToHash using SHA-1
             MessageDigest sha = MessageDigest.getInstance("SHA-1");
             byte[] hashedBytes = sha.digest(textToHash.getBytes());
+
+            // Specify possible digits for the output hash
             char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                 'a', 'b', 'c', 'd', 'e', 'f'};
+
+            // For each hashed byte, select two digits to be added to the output hash
             for (int i = 0; i < hashedBytes.length; i++) {
                 byte b = hashedBytes[i];
                 hash.append(digits[(b & 0xf0) >> 4]);
