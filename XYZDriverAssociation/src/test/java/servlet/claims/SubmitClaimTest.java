b7c49d97da91d2023ff346bc5843c30adbaf4221
Michael McCormick <MichaelM97@live.co.uk>
2019-11-23 17:31:16 +0000

    Claims (#19)
    * Added logic to allow user to create a new claim
    * Created ListClaims Servlet to allow the admin to view all claims in the DB
    * Updated dashboard JSP's to point to the new servlets
    * Added required dependencies for new tests
    * Created Test classes for claims Servlets
    * Removed duplicate import in HashHelperTest
    * Deleted fake unit test class
    * Specified which package of unit tests to run in the CircleCi config

---
+++ XYZDriverAssociation/src/test/java/servlet/claims/SubmitClaimTest.java
@@ -0,0 +1,56 @@
+package servlet.claims;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Claim;
+import model.User;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class SubmitClaimTest extends Mockito {
+
+    @Test
+    public void shouldRedirectToHomeWhenNullUser() throws Exception {
+        // Given
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        HttpSession session = mock(HttpSession.class);
+        when(session.getAttribute("user")).thenReturn(null);
+        when(request.getSession(false)).thenReturn(session);
+
+        // When
+        new SubmitClaim().doPost(request, response);
+
+        // Then
+        verify(session, times(1)).getAttribute("user");
+        verify(request, times(1)).getSession(false);
+        verify(response, times(1)).sendRedirect("./home.jsp");
+    }
+
+    @Test
+    public void shouldInsertClaimObjectWhenValidPost() throws Exception {
+        // Given
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        HttpSession session = mock(HttpSession.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        User user = new User("Michael", "password", "APPROVED");
+        when(session.getAttribute("user")).thenReturn(user);
+        when(request.getSession(false)).thenReturn(session);
+        when(request.getRequestDispatcher("claims/submit_claim.jsp")).thenReturn(dispatcher);
+        when(request.getParameter("description")).thenReturn("I crashed my car.");
+        when(request.getParameter("amount")).thenReturn("99.99");
+
+        // When
+        new SubmitClaim().doPost(request, response);
+
+        // Then
+        verify(session, times(1)).getAttribute("user");
+        verify(request, times(1)).getSession(false);
+        verify(request, times(1)).getParameter("description");
+        verify(request, times(1)).getParameter("amount");
+        verify(request, times(1)).setAttribute(eq("createdClaim"), any(Claim.class));
+    }
+}
