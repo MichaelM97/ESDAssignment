f307536a1232009dc2494fdf924c2c4b4267b6b7
Michael McCormick <MichaelM97@live.co.uk>
2019-11-28 13:07:57 +0000

    * Created unit tests for both filters * Removed user auth verification from old unit tests

--- XYZDriverAssociation/src/test/java/servlet/claims/SubmitClaimTest.java
+++ XYZDriverAssociation/src/test/java/servlet/claims/SubmitClaimTest.java
@@ -9,49 +9,43 @@ import javax.servlet.http.HttpSession;
 import model.Claim;
 import model.User;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
 
 public class SubmitClaimTest extends Mockito {
 
-    @Test
-    public void shouldRedirectToHomeWhenNullUser() throws Exception {
-        // Given
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        HttpSession session = mock(HttpSession.class);
-        when(session.getAttribute("user")).thenReturn(null);
-        when(request.getSession(false)).thenReturn(session);
+    private static final String JSP = "claims/submit_claim.jsp";
 
-        // When
-        new SubmitClaim().doPost(request, response);
+    private static HttpServletRequest request;
+    private static HttpServletResponse response;
+    private static RequestDispatcher requestDispatcher;
+    private static HttpSession session;
 
-        // Then
-        verify(session, times(1)).getAttribute("user");
-        verify(request, times(1)).getSession(false);
-        verify(response, times(1)).sendRedirect("./home.jsp");
+    @Before
+    public void setUp() {
+        request = mock(HttpServletRequest.class);
+        response = mock(HttpServletResponse.class);
+        requestDispatcher = mock(RequestDispatcher.class);
+        session = mock(HttpSession.class);
     }
 
     @Test
     public void shouldInsertClaimObjectWhenValidPost() throws Exception {
         // Given
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        HttpSession session = mock(HttpSession.class);
-        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
         User user = new User(
-                "MichaelM12",
-                "password",
-                "Michael McCormick",
+                "Mike123",
+                "Michael",
                 "50 The Meadows",
                 new Date(),
                 new Date(),
-                0.0f,
-                User.STATUS_APPROVED
+                100.0f,
+                User.STATUS_PENDING
         );
         when(session.getAttribute("user")).thenReturn(user);
         when(request.getSession(false)).thenReturn(session);
-        when(request.getRequestDispatcher("claims/submit_claim.jsp")).thenReturn(dispatcher);
+        when(request.getRequestDispatcher(JSP)).thenReturn(requestDispatcher);
         when(request.getParameter("description")).thenReturn("I crashed my car.");
         when(request.getParameter("amount")).thenReturn("99.99");
 
@@ -59,11 +53,9 @@ public class SubmitClaimTest extends Mockito {
         new SubmitClaim().doPost(request, response);
 
         // Then
-        verify(session, times(1)).getAttribute("user");
-        verify(request, times(1)).getSession(false);
-        verify(request, times(1)).getParameter("description");
-        verify(request, times(1)).getParameter("amount");
-        verify(request, times(1)).setAttribute(eq("createdClaim"), any(Claim.class));
+        verify(request).getParameter("description");
+        verify(request).getParameter("amount");
+        verify(request).setAttribute(eq("createdClaim"), any(Claim.class));
     }
 
     @After
